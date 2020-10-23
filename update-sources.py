fecfb047ad1fa8c98f8fcf683009b6f91758748b
Joshua Stone <joshua.gage.stone@gmail.com>
2020-10-23 13:34:40 -0400

    Add preliminary publishing date extraction

--- update-sources.py
+++ update-sources.py
@@ -4,6 +4,8 @@ import json
 import requests
 import sys
 from Crypto.Hash import SHA256
+# https://pypi.org/project/python-dateutil/
+import dateutil.parser
 
 url = 'https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json'
 destination = 'sources.json'
@@ -40,3 +42,14 @@ print(json.dumps(content, sort_keys=True, indent=4))
 
 with open(destination, 'w') as f:
     json.dump(content, f, sort_keys=True, indent=4)
+
+
+posts = requests.get('https://forum.obsidian.md/c/announcements/13.json').json()
+
+formatted_version = f"obsidian-release-v{version.replace('.','-')}"
+
+for post in posts['topic_list']['topics']:
+    if post['slug'] == formatted_version:
+        post_date = dateutil.parser.parse(post['created_at'])
+        break
+
