ecf8b01af19863aa9395a16df9135f4afd083f5d
Joshua Stone <joshua.gage.stone@gmail.com>
2021-05-14 14:49:33 -0400

    Add an environment variable for launching Obsidian under Wayland

---
+++ obsidian.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -oue pipefail
+
+export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+
+# Wayland support can be optionally enabled like so:
+# flatpak override --user --env=OBSIDIAN_USE_WAYLAND=1 md.obsidian.Obsidian
+declare -i OBSIDIAN_USE_WAYLAND="${OBSIDIAN_USE_WAYLAND:-0}"
+declare -i EXIT_CODE=0
+
+if [[ "${OBSIDIAN_USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
+    zypak-wrapper /app/obsidian --enable-features=UseOzonePlatform --ozone-platform=wayland $@ || EXIT_CODE=$?
+    # Fall back to x11 if Obsidian failed to launch under Wayland. Otherwise, exit normally
+    [[ "${EXIT_CODE}" -ne 133 ]] && exit "${EXIT_CODE}"
+fi
+
+zypak-wrapper /app/obsidian $@
