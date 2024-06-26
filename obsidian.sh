84f0a466e3521a2bffe89f61d87a101f7e3c1a7a
5l2 <48936628+5l2@users.noreply.github.com>
2024-06-26 16:08:50 +0800

    Reverse the change of adding the `--gtk-version=4` option, as it did not fix the problem.

--- obsidian.sh
+++ obsidian.sh
@@ -29,7 +29,6 @@ if [[ -e "${XDG_RUNTIME_DIR}/${WL_DISPLAY}" || -e "/${WL_DISPLAY}" ]]; then
         --ozone-platform-hint=auto
 	--enable-features=WaylandWindowDecorations
 	--enable-wayland-ime
-	--gtk-version=4
     )
     if [[ -c /dev/nvidia0 ]]; then
         echo "Debug: Detecting Nvidia GPU. disabling GPU sandbox."
